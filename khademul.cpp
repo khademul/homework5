6fcb6559489e54262b5ee7905e79675910c48832
khademul <khademul144@gmail.com>
2015-03-01 14:53:00 -0600

    version 5.0
    sample code as a starting point for homework 5

---
+++ khademul.cpp
@@ -0,0 +1,28 @@
+/* Homework 5
+   CIVL 8903
+   Khademul Haque
+   URL:
+*/
+//sample code as a start for homework 5
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int a,b;
+
+int main (){
+	cout<<"enter a= ";
+	cin>>a;
+
+	cout<<"enter b= ";
+	cin>>b;
+
+	cout<<a+b<<endl;
+
+	system ("pause");
+	return 0;
+}
+
